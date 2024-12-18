ca2d0526d8f51162977de1bbe3ec57b618c05ef9
Žaneta Martinů <zaneta.martinu@vdimaging.com>
2024-12-18 13:28:09 +0100

    Edit check if rules.

--- main.py
+++ main.py
@@ -47,25 +47,37 @@ if __name__ == '__main__':
         update = update.split(',')
         update = list(map(int, update))
         correct = True
-        for u in range(0, len(update)):
-            print(update)
-            number = update[u]
-            print(number)
 
-            for rule in rules:
-                # print(rule)
-                X = int(rule[0:2])
-                Y = int(rule[3:5])
-                # print(X)
-                # print(Y)
-                # print()
-                if X == number:
-                    # index_X = update.index(X)
-                    # index_Y = update.index(Y)
-                    # index_X < index_Y
-                    if not is_before(update, X, Y):
-                        correct = False
+        for rule in rules:
+            X = int(rule[0:2])
+            Y = int(rule[3:5])
+
+            if X in update and Y in update:
+                if not is_before(update, X, Y):
+                    correct = False
+                    # End cycle if at least one mistake
+                    break
         if correct:
-            s += 1
+            valid += 1
+        # for u in range(0, len(update)):
+        #     print(update)
+        #     number = update[u]
+        #     print(number)
+        #
+        #     for rule in rules:
+        #         # print(rule)
+        #         X = int(rule[0:2])
+        #         Y = int(rule[3:5])
+        #         # print(X)
+        #         # print(Y)
+        #         # print()
+        #         if X == number:
+        #             # index_X = update.index(X)
+        #             # index_Y = update.index(Y)
+        #             # index_X < index_Y
+        #             if not is_before(update, X, Y):
+        #                 correct = False
+        # if correct:
+        #     s += 1
 
-    print('vysledek', s)
+    print('vysledek', valid)
