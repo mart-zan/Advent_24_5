0d88b1b98e95cbfb0bc4c0d656ac028dbdbb4f16
Žaneta Martinů <zaneta.martinu@vdimaging.com>
2024-12-18 11:20:57 +0100

    First comit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+# Advent of Code 2024
+# --- Day 5: Print Queue ---
+
+
+def read_input_by_rows(filename: str):
+
+    my_file = open(filename, "r")
+    data = my_file.read()
+    # Split text into each row
+    rows = data.strip().split('\n')
+    return rows
+
+
+if __name__ == '__main__':
+    print('PyCharm')
+
