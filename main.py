b5ad5899450da55b2a8800c0336446155dd2037b
Žaneta Martinů <zaneta.martinu@vdimaging.com>
2024-12-18 11:35:24 +0100

    read input

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ def read_input_by_rows(filename: str):
 if __name__ == '__main__':
     print('PyCharm')
 
+    # Read input
+    rules_updates = read_input_by_rows('input.txt')
+
+    # Separate rules and updates
+    bool_separate = [len(ele) == 0 for ele in rules_updates]
+    idx_separate = [i for i, x in enumerate(bool_separate) if x]
+    rules = rules_updates[:(idx_separate[0])]
+    updates = rules_updates[(idx_separate[0]+1):]
+    # print(len(rules))
+    # print(len(updates))
+    # print(rules[0], ', ', rules[-1])
+    # print(updates[0], ', ', updates[-1])
+
+    
