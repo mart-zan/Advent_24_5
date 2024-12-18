6a7589e3710107a51d376a0d1857c98600cfa7c4
Žaneta Martinů <zaneta.martinu@vdimaging.com>
2024-12-18 13:23:51 +0100

    Edit initializaton.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@
 
 def read_input_by_rows(filename: str):
 
-    my_file = open(filename, "r")
-    data = my_file.read()
+    with open(filename, "r") as my_file:
+        data = my_file.read()
     # Split text into each row
     rows = data.strip().split('\n')
     return rows
@@ -20,34 +20,38 @@ def is_before(lst, X, Y):
         # Check if X is before Y
         return index_X < index_Y
     except ValueError:
-        # If X or Y is not in the list, return False
+        # If X or Y is not in the list
         return False
 
 
 if __name__ == '__main__':
-    print('PyCharm')
 
     # Read input
     rules_updates = read_input_by_rows('input.txt')
 
     # Separate rules and updates
-    bool_separate = [len(ele) == 0 for ele in rules_updates]
-    idx_separate = [i for i, x in enumerate(bool_separate) if x]
-    rules = rules_updates[:(idx_separate[0])]
-    updates = rules_updates[(idx_separate[0]+1):]
+    # bool_separate = [len(ele) == 0 for ele in rules_updates]
+    idx_separate = rules_updates.index('')
+    # idx_separate = int(idx_separate)
+    print(idx_separate)
+    rules = rules_updates[:idx_separate]
+    updates = rules_updates[(idx_separate+1):]
     # print(len(rules))
     # print(len(updates))
     # print(rules[0], ', ', rules[-1])
     # print(updates[0], ', ', updates[-1])
-
+    # correct = 0
+    valid = 0
     for update in updates:
         print(update)
-        update = update.split(r',')
+        update = update.split(',')
         update = list(map(int, update))
+        correct = True
         for u in range(0, len(update)):
             print(update)
             number = update[u]
             print(number)
+
             for rule in rules:
                 # print(rule)
                 X = int(rule[0:2])
@@ -59,6 +63,9 @@ if __name__ == '__main__':
                     # index_X = update.index(X)
                     # index_Y = update.index(Y)
                     # index_X < index_Y
-                    # check = is_before(update, update, )
+                    if not is_before(update, X, Y):
+                        correct = False
+        if correct:
+            s += 1
 
-                    print('update')
+    print('vysledek', s)
